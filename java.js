a33b70211ef818e43e6fe1c7e5bf11ac81f39ae4
Marzia Nazami <marzianazami16@gmail.com>
2026-08-29 13:55:08 +0430

    Add last sction code

--- java.js
+++ java.js
@@ -187,3 +187,28 @@ function updateClock(){
 
 setInterval(updateClock,1000);
 
+
+window.addEventListener("load",()=>{
+
+    document
+    .querySelectorAll(".flight-card")
+    .forEach((card,i)=>{
+
+        card.style.opacity=0;
+
+        card.style.transform="translateY(30px)";
+
+        setTimeout(()=>{
+
+            card.style.transition=".5s";
+
+            card.style.opacity=1;
+
+            card.style.transform="translateY(0)";
+
+        },i*150);
+
+    });
+
+});
+
