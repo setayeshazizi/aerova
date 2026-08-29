cf075e2b72091cc54d695e69b1fcd73ac14113c7
AzadehAnwari <anwari.a642@gmail.com>
2026-08-29 09:53:22 +0430

    date format

--- booking.js
+++ booking.js
@@ -503,3 +503,20 @@ function goToStep(step) {
     behavior: "smooth",
   });
 }
+/* =========================================
+   DATE FORMAT
+========================================= */
+
+function formatDate(dateString) {
+  if (!dateString) {
+    return "---";
+  }
+
+  const date = new Date(dateString + "T00:00:00");
+
+  return date.toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
