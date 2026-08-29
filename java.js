f8c4913b9251df7d115d753e7b4f2690a5dc1488
Marzia Nazami <marzianazami16@gmail.com>
2026-08-29 13:48:21 +0430

    flight card java

---
+++ java.js
@@ -0,0 +1,46 @@
+
+const flights = [
+  {
+    airline: "Swiss Air",
+    code: "LX 8001",
+    from: "Paris (PAR)",
+    to: "Zurich (ZRH)",
+    depart: "08:45 AM",
+    arrive: "10:00 AM",
+    duration: "1h 15m",
+    price: 185
+  },
+
+  {
+    airline: "Air France",
+    code: "AF 1208",
+    from: "Paris (PAR)",
+    to: "Zurich (ZRH)",
+    depart: "12:30 PM",
+    arrive: "01:50 PM",
+    duration: "1h 20m",
+    price: 210
+  },
+
+  {
+    airline: "EasyJet",
+    code: "EJ 4412",
+    from: "Paris (PAR)",
+    to: "Zurich (ZRH)",
+    depart: "04:15 PM",
+    arrive: "05:40 PM",
+    duration: "1h 25m",
+    price: 95
+  },
+
+  {
+    airline: "Lufthansa",
+    code: "LH 650",
+    from: "Paris (PAR)",
+    to: "Zurich (ZRH)",
+    depart: "06:10 PM",
+    arrive: "07:30 PM",
+    duration: "1h 20m",
+    price: 165
+  }
+];
